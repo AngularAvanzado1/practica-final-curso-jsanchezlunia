f78bb30d05d05a104c5df9a926de818f3b89e7f2
Juan Francisco Sánchez Aldeguer <jsanchez@lunia.es>
2020-12-14 09:03:00 +0100

    Arreglados tests

--- apps/explorador-continentes/src/app/listado-paises-region/listado-paises-region.component.spec.ts
+++ apps/explorador-continentes/src/app/listado-paises-region/listado-paises-region.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ListadoPaisesRegionComponent } from './listado-paises-region.component';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
 import {RouterTestingModule} from '@angular/router/testing';
+import {provideMockStore} from '@ngrx/store/testing';
 
 describe('ListadoPaisesRegionComponent', () => {
   let component: ListadoPaisesRegionComponent;
@@ -11,7 +12,10 @@ describe('ListadoPaisesRegionComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ ListadoPaisesRegionComponent ],
-      imports: [RouterTestingModule, HttpClientTestingModule]
+      imports: [RouterTestingModule, HttpClientTestingModule],
+      providers: [
+        provideMockStore()
+      ]
     })
     .compileComponents();
   });
--- apps/explorador-continentes/src/app/region/store/paises/paises.service.spec.ts
+++ apps/explorador-continentes/src/app/region/store/paises/paises.service.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { PaisesFacade } from './paises.service';
+import {provideMockStore} from '@ngrx/store/testing';
 
 describe('PaisesFacade', () => {
   let service: PaisesFacade;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        provideMockStore()
+      ]
+    });
     service = TestBed.inject(PaisesFacade);
   });
 
