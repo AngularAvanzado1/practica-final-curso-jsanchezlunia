b847d81de743e901e0a042134d1bfc809ea8dd64
Juan Francisco Sánchez Aldeguer <jsanchez@lunia.es>
2020-12-16 13:15:39 +0100

    Generada vanila, una aplicación web estándar (sin Angular) que usa el web component wbde-country de Angular

--- apps/wbde-country/src/app/app.module.ts
+++ apps/wbde-country/src/app/app.module.ts
@@ -3,9 +3,10 @@ import {Injector, NgModule} from '@angular/core';
 import {CountryComponent, CountryModule} from '@pca-jsanchez/country';
 import {createCustomElement} from '@angular/elements';
 import 'zone.js';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
-  imports: [BrowserModule, CountryModule],
+  imports: [BrowserModule, HttpClientModule, CountryModule],
   entryComponents: [CountryComponent]
 })
 export class AppModule {
